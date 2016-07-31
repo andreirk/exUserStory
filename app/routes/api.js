053296da1dacd3116dff9269f6fa9bcfef0f6924
an.sokolov <sokolik_85@list.ru>
2016-07-31 12:46:10 +0700

    add login api, jwt token auth

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,7 +1,20 @@
 var User = require('../models/user');
 var config = require('../../config');
-
+var jwt = require('jsonwebtoken');
 var secretKey = config.secretKey;
+
+
+function createToken(user){
+     var token = jwt.sign({
+         id: user.id,
+         username: user.username,
+         name: user.name
+     }, secretKey,{
+         expiresIn: '120m'
+     });
+
+     return token;
+}
 
 module.exports = function(app, express) {
     api = express.Router();
@@ -32,12 +45,38 @@ module.exports = function(app, express) {
                 return;
             }
             res.json(users);
+        });
+    });
+
+    api.post('/login', function(req, res){
+
+        User.findOne({
+            username: req.body.username,
+        }).select('password').exec(function(err, user){
+            if(err){
+                throw err;
+            }
+            if(!user){
+                res.send({message: 'User doesnt exist'});
+            } else {
+                var validPassword = user.comparePassword(req.body.password);
+
+                if(!validPassword){
+                    res.send({message: 'invalid Password'});
+                } else {
+                    ///// token
+                    var token = createToken(user);
+
+                    res.json({
+                        success:true,
+                        message: 'Successfuli login',
+                        token: token
+                    })
+
+                }
+            }
         })
-    })
-
-
-
-
+    });
 
     return api;
 };
